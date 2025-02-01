61ea5a688ffb5431146462a3ae7ee51fbdda6bd7
= <alexey2435ytgh@gmail.com>
2025-02-01 07:30:40 +0300

    Создание массива организаций

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Список организаций</h1>
+    <ul>
+      <li v-for="(org, index) in organizations" :key="index">
+        {{ org.name }} - {{ org.director }} - {{ org.phone }}
+      </li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      organizations: [
+        { name: 'Организация 1', director: 'Иванов Иван Иванович', phone: '123-456' },
+        { name: 'Организация 2', director: 'Петров Петр Петрович', phone: '789-012' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
